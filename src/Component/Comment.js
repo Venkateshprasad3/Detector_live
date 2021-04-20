c2a56231bff8202b3501b23e258d1a39051b75b5
Venkateshprasad3 <venkateshprasad310@gmail.com>
2021-04-20 11:16:14 +0530

    ip

--- src/Component/Comment.js
+++ src/Component/Comment.js
@@ -6,10 +6,33 @@ import { db } from '../firebase';
     
   
       const Contact =()=> {
+
+    const [details,setDetails]=useState("");
+   
+// 
+  
+//     alert(details.IPv4);
+
+// }
+const getGeoLocationDetails= ()=>{
+    fetch("https://geolocation-db.com/json/afa4d000-8eb9-11eb-a6ff-2538b793e762")
+    .then(response =>response.json())
+    .then(data=>setDetails(data))
+    }
+
+
     const [name,SetName]=useState("");
     const handleSubmit=(e)=>{
         e.preventDefault();
+        getGeoLocationDetails();
+       
+
+           var ipa=details.IPv4;
+
+
+
         db.collection('comments').add({
+            ip:ipa,
             comments:name
         })
         .then(()=>{
@@ -27,6 +50,7 @@ import { db } from '../firebase';
         <center>
             <textarea value={name} onChange={(e)=>SetName(e.target.value)}></textarea>
             <br/>
+            
             <button onClick={handleSubmit}>Submit</button>
         
         </center>
